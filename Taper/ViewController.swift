36b762587498574cde5cd11a53e385b67ba58dea
Zach Waugh <zwaugh@gmail.com>
2021-05-06 11:47:55 -0400

    Initial Commit

---
+++ Taper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Taper
+//
+//  Created by Zach Waugh on 5/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
