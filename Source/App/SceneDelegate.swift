8f4d3abd498f9d926c92580a1652500eb8f17453
Zach Waugh <zwaugh@gmail.com>
2021-05-18 13:25:58 -0400

    Add document browser boilerplate

--- Source/App/SceneDelegate.swift
+++ Source/App/SceneDelegate.swift
@@ -4,10 +4,6 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
 
-        let listViewController = DocumentsViewController(store: DocumentStore.shared)
-        let navController = UINavigationController(rootViewController: listViewController)
-        window?.rootViewController = navController
     }
 }
